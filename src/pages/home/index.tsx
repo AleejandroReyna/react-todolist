658d634839def01896a847e8f92ead2874a0c3e3
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-03-05 00:31:39 -0600

    Added bootstrap icons and jumbotron in home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import {
+    Jumbotron,
+    Button
+} from 'react-bootstrap'
+import {
+    Github
+} from 'react-bootstrap-icons'
 
 const Home = () => {
-    return <section>Home!</section>
+    return (
+        <section>
+            <Jumbotron>            
+                <h1 className='display-4'>React Todolist</h1>
+                <p className='lead'>This is a simplet Todolist project with react! </p>
+                <hr className="my-4"/>
+                <p>It's Working with <a href="https://github.com/AleejandroReyna/django-todolist" target='_blank'>django Todolist</a>!</p>
+                <Button href="" variant="primary">
+                    <Github /> Go to Github
+                </Button>
+            </Jumbotron>
+        </section>
+    )
 }
 
 export default Home
