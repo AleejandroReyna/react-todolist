41ba903102cf5ccf90fec36bcdeaef644a8eeafa
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-03-08 13:10:24 -0600

    Added panel in dashboard

--- src/components/taskPanel/index.tsx
+++ src/components/taskPanel/index.tsx
@@ -5,6 +5,9 @@ const TaskPanel = ({type, label}: TaskPanelProps) => {
     return (
         <article>
             <h5>{label}</h5>
+            <section className="bg-light border rounded rounded-3 p-2">
+                tasks drag and drop panel
+            </section>
         </article>
     )
 }
